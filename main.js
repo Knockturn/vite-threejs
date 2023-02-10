38707a2eb80b5c566c589566a75bfd9f2c47d7c8
Knockturn <pkbrauner@gmail.com>
2023-02-10 20:35:07 +0100

    music added

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ container.appendChild(renderer.domElement);
 const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x86a3d8);
+scene.background = new THREE.Color(0x21252c);
 scene.environment = pmremGenerator.fromScene(
   new RoomEnvironment(),
   0.04
@@ -44,6 +44,20 @@ camera.position.set(0, 0, 10);
 // controls.update();
 // controls.enablePan = false;
 // controls.enableDamping = true;
+
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add(listener);
+// create a global audio source
+const sound = new THREE.Audio(listener);
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("sounds/ambient.mp3", function (buffer) {
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.setVolume(0.5);
+  sound.play();
+});
 
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath("jsm/libs/draco/");
